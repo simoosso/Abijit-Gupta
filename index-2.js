fceef1dd9c488e4bbe08bf9cfaae9afe927ce4ba
simoosso <44887215+simoosso@users.noreply.github.com>
2021-08-06 22:47:16 -0700

    Update index-2.js

--- index-2.js
+++ index-2.js
@@ -1,1 +1,4 @@
-your server is out down please contact your administrator.  https://www.systemviral.com/
+
+<div style='text-align: center;'>
+<div class='lalulintas1'><a href='#'><span class='rambuhijau1'> your server is out down please contact your administrator.  https://www.systemviral.com/ </span></a></div>
+</div>
